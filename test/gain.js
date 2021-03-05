630676d5a93a3f160fbeeb977a48d57cd4a90eee
Phil Deschaine <deschai5@gmail.com>
2021-03-05 18:02:02 +0800

    more gain tests

--- test/gain.js
+++ test/gain.js
@@ -34,8 +34,66 @@ test("a gain", async (t) => {
   await delay(300)
   value = await redis.get("status__a__gain")
   t.is(value, "127")
-  redis_pub.publish("purple-sector", "command__a__gain__20")
+  redis_pub.publish("purple-sector", "command__a__gain__63")
   await delay(300)
   value = await redis.get("status__a__gain")
+  t.is(value, "63")
+})
+
+test("b gain", async (t) => {
+  const { redis, redis_pub } = t.context
+  let value
+
+  redis_pub.publish("purple-sector", "command__b__gain__0")
+  await delay(300)
+  redis_pub.publish("purple-sector", "command__b__gain__20")
+  await delay(300)
+  value = await redis.get("status__b__gain")
   t.is(value, "20")
+  redis_pub.publish("purple-sector", "command__b__gain__127")
+  await delay(300)
+  value = await redis.get("status__b__gain")
+  t.is(value, "127")
+  redis_pub.publish("purple-sector", "command__b__gain__63")
+  await delay(300)
+  value = await redis.get("status__b__gain")
+  t.is(value, "63")
+})
+test("c gain", async (t) => {
+  const { redis, redis_pub } = t.context
+  let value
+
+  redis_pub.publish("purple-sector", "command__c__gain__0")
+  await delay(300)
+  redis_pub.publish("purple-sector", "command__c__gain__20")
+  await delay(300)
+  value = await redis.get("status__c__gain")
+  t.is(value, "20")
+  redis_pub.publish("purple-sector", "command__c__gain__127")
+  await delay(300)
+  value = await redis.get("status__c__gain")
+  t.is(value, "127")
+  redis_pub.publish("purple-sector", "command__c__gain__63")
+  await delay(300)
+  value = await redis.get("status__d__gain")
+  t.is(value, "63")
+})
+test("d gain", async (t) => {
+  const { redis, redis_pub } = t.context
+  let value
+
+  redis_pub.publish("purple-sector", "command__d__gain__0")
+  await delay(300)
+  redis_pub.publish("purple-sector", "command__d__gain__20")
+  await delay(300)
+  value = await redis.get("status__d__gain")
+  t.is(value, "20")
+  redis_pub.publish("purple-sector", "command__d__gain__127")
+  await delay(300)
+  value = await redis.get("status__d__gain")
+  t.is(value, "127")
+  redis_pub.publish("purple-sector", "command__d__gain__63")
+  await delay(300)
+  value = await redis.get("status__d__gain")
+  t.is(value, "63")
 })
